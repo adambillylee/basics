adba923bcfa8fbfb1ebc5f7c3b5c6ee2d0327891
Adam Li <adam.li@ensighten.com>
2016-03-13 22:24:22 -0700

    * permutation with dedup II

---
+++ src/Permutation_with_dedup_II.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by adamli on 3/13/16.
+ */
+public class Permutation_with_dedup_II {
+    public static void permutate(char[] input, int index) {
+        if (index == input.length) {
+            System.out.println(input);
+            return;
+        }
+
+        HashSet<Character> visited = new HashSet<>();
+        for (int i=index; i<input.length; i++) {
+            if (!visited.contains(input[i])) {
+                visited.add(input[i]);
+                swap(input, i, index);
+                permutate(input, index+1);
+                swap(input, i, index);
+            }
+        }
+    }
+
+    public static void swap(char[] input, int i, int j) {
+        char tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+}
---
+++ test/PermutationWithdedupIItest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by adamli on 3/13/16.
+ */
+public class PermutationWithdedupIItest {
+
+    @Test
+    public void testPermutate() throws Exception {
+        char[] input = {'a','a','b'};
+        Permutation_with_dedup_II.permutate(input, 0);
+    }
+}
