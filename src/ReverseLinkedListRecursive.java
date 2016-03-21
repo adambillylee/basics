56b6fbd6d5b22bc48709d0c1ac7e6112a42e6772
Adam Li <adam.li@ensighten.com>
2016-03-21 00:50:15 -0700

    * add comments

--- src/ReverseLinkedListRecursive.java
+++ src/ReverseLinkedListRecursive.java
@@ -10,16 +10,18 @@ public class ReverseLinkedListRecursive {
         /**
          * reversed head node
          * with next link all the way to next node
+         * 2<-3<-4<-5....
          */
         ListNode reverseHead = reverse(node.next);
 
         /**
          * with reverse head -> next node
          * fix next node to current node link
+         * 1<-2
          */
         node.next.next = node;
 
-        // curr node should point to null
+        // curr node should point to null (null<-1)
         node.next = null;
 
         // still return that reversed had node to next level
