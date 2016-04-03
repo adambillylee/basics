ba166e77237c501c01f9624ff0fea8b5c364d148
Adam Li <adam.li@ensighten.com>
2016-04-03 00:30:47 -0700

    add comments

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -7,14 +7,28 @@ public class WordBreak {
     public static boolean wordBreak(String s, Set<String> wordDict) {
         /**
          * m represent from cut position 0~i, can s be divided into dict words
+         * cut position index = string index + 1;
          */
         boolean[] m = new boolean[s.length() + 1];
         m[0] = true;
 
+        /**
+         * BUG POINT:
+         * cut position = string index + 1
+         */
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
+
+                /**
+                 * BUG POINT:
+                 * cut position = string index + 1
+                 */
                 String partial = s.substring(j, i);
 
+                /**
+                 * if we cut at position j, before j is in dict
+                 * than if j~i is in dict, i is in dict
+                 */
                 if (m[j] && wordDict.contains(partial)) {
                     m[i] = true;
                     break;
