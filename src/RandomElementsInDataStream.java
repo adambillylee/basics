650ea0e33cfe83cfc15d33e60d787464b515eab6
Adam Li <adam.li@ensighten.com>
2016-04-12 00:30:29 -0700

    random elements in data stream

--- src/RandomElementsInDataStream.java
+++ src/RandomElementsInDataStream.java
@@ -5,18 +5,18 @@ import java.util.Random;
  */
 public class RandomElementsInDataStream {
     public static int[] randomElement(int[] input) {
-        int curr = input[0];
         int[] rst = new int[input.length];
+        rst[0] = input[0];
 
         Random rand = new Random();
 
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
             int random = rand.nextInt(i+1);
 
-            if (random != 0)
-                curr = input[random];
+            if (random == 0)
+                rst[i] = rst[i-1];
 
-            rst[i] = curr;
+            rst[i] = input[random];
         }
 
         return rst;
