f4cfc09af57302b535fdcc9cf5bfd6bc9a07dee1
Adam Li <adam.li@ensighten.com>
2016-05-24 20:00:04 -0700

    * max prod of string lengths

--- src/MaximumProductOfWordLengths.java
+++ src/MaximumProductOfWordLengths.java
@@ -13,13 +13,20 @@ public class MaximumProductOfWordLengths {
         for (int i = 0; i < words.length; i++) {
             char[] charArray = words[i].toCharArray();
             for (char c : charArray) {
+                /**
+                 * 32 bits integer has 32 pos in binary
+                 * more 1 to pos that represent a char for each char
+                 */
                 mask[i] |= 1 << c - 'a';
             }
         }
 
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = i + 1; j < words.length; j++) {
-                // use mask for comparison
+                /**
+                 * use bit "and" for O(1) comparison
+                 * if no char in common, all binary position will be zero
+                 */
                 if ((mask[i] & mask[j]) == 0)
                     max = Math.max(max, words[i].length() * words[j].length());
             }
