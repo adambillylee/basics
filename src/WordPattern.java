922363d427f0ebc8509fe1e241f802417d442b56
adam lee <adambillylee@gmail.com>
2016-05-30 02:43:08 -0700

    * comment update

--- src/WordPattern.java
+++ src/WordPattern.java
@@ -12,16 +12,26 @@ public class WordPattern {
         if (pattern.length() != arr.length)
             return false;
 
+        /**
+         * loop through pattern and word together
+         * matching pairs: a char in pattern => a word in str
+         */
         for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
             String word = arr[i];
 
             if (!map.containsKey(c)) {
+                /**
+                 * if current word is assigned to another char in pattern, return false
+                 */
                 if (map.containsValue(word))
                     return false;
 
                 map.put(c, word);
             } else {
+                /**
+                 * if word corresponding to c is not equal to current word, return false
+                 */
                 if (!map.get(c).equals(word))
                     return false;
             }
