a5b3b7d2c10f25ea93d6831b9c1df20f0557ca9c
Adam Li <adam.li@ensighten.com>
2016-06-07 00:50:39 -0700

    * Google OA -> find max possible duplicate number in one pass

---
+++ test/FindLargestDuplicateOnePassTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by adamli on 6/7/16.
+ */
+public class FindLargestDuplicateOnePassTest {
+    FindLargestDuplicateOnePass sol = new FindLargestDuplicateOnePass();
+
+    @Test
+    public void findLargestDup() throws Exception {
+        sol.FindLargestDup(12345);
+    }
+
+}
