1c1ffb8a4500d210f7301bebb69378b762f8a03e
Adam Li <adam.li@ensighten.com>
2016-06-06 23:53:47 -0700

    * Google OA -> find max possible duplicate number

---
+++ test/FindLargestDuplicateTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by adamli on 6/6/16.
+ */
+public class FindLargestDuplicateTest {
+    FindLargestDuplicate sol = new FindLargestDuplicate();
+
+    @Test
+    public void findLargestDup() throws Exception {
+        System.out.println(sol.FindLargestDup(12345));
+    }
+
+}
