459d3546215e26366f7fae515749af874cf07ba7
Adam Li <adam.li@ensighten.com>
2016-06-25 23:27:19 -0700

    * add sanity check to paint fence

--- src/PaintFence.java
+++ src/PaintFence.java
@@ -2,7 +2,7 @@
  * Created by adamli on 6/23/16.
  */
 public class PaintFence {
-    public int numWays(int n, int k) {
+    public int numWaysON(int n, int k) {
         int[] same2 = new int[n + 1];
         int[] diff2 = new int[n + 1];
 
@@ -15,5 +15,26 @@ public class PaintFence {
         }
 
         return same2[n] + diff2[n];
+    }
+
+    public int numWays(int n, int k) {
+        // if there is no fence or color, there is 0 ways to paint
+        if (n == 0 || k == 0)
+            return 0;
+
+        // if there is only one fence, there are k ways to paint
+        if (n == 1)
+            return k;
+
+        int same2 = k;
+        int diff2 = k * (k - 1);
+
+        for (int i = 2; i < n; i++) {
+            int tmp = same2;
+            same2 = diff2;
+            diff2 = tmp * (k-1) + diff2 * (k-1);
+        }
+
+        return same2 + diff2;
     }
 }
