d60e7b06d77a2c18ec076e26e2462202ee9826ee
Adam Li <adam.li@ensighten.com>
2016-06-28 00:59:41 -0700

    * code clean up

--- test/QuickSelectTest.java
+++ test/QuickSelectTest.java
@@ -1,9 +1,6 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by adamli on 6/27/16.
  */
@@ -17,18 +14,18 @@ public class QuickSelectTest {
 
         for (int i = 0; i < test.length; i++) {
             int tmp = sorted[i];
-            Assert.assertEquals(sol.quickSelect(test, i+1), tmp);
+            Assert.assertEquals(sol.quickSelect(test, i + 1), tmp);
         }
     }
 
     @Test
     public void testSelectWithDup() {
-        int[] test = new int[]{7, 7, 2, 3, 5, 1, 4};
+        int[] test = new int[]{7, 5, 2, 3, 7, 1, 4};
         int[] sorted = {1, 2, 3, 4, 5, 7, 7};
 
         for (int i = 0; i < test.length; i++) {
             int tmp = sorted[i];
-            Assert.assertEquals(sol.quickSelect(test, i+1), tmp);
+            Assert.assertEquals(sol.quickSelect(test, i + 1), tmp);
         }
     }
 
