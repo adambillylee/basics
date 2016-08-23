86a016b0aa6d4a6cb9e85b9580f3b1eac27e0dbe
Adam Li <adam.li@ensighten.com>
2016-08-22 22:11:55 -0700

    * recover bst

---
+++ java/RecoverBST.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+
+/**
+ * Created by adamli on 8/22/16.
+ */
+public class RecoverBST {
+    private Stack<TreeNode> stack = new Stack<>();
+
+    TreeNode peak1, valley1, peak2, valley2, last = null;
+
+    public void recoverTree(TreeNode root) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+
+        while(!stack.isEmpty()) {
+            curr = stack.pop();
+
+            TreeNode right = curr.right;
+            while(right != null) {
+                stack.push(right);
+                right = right.left;
+            }
+
+            // find two peaks and two valleys
+            if (last != null || last.val >= curr.val) {
+                if (peak1 == null) {
+                    peak1 = last;
+                    valley1 = curr;
+                }else{
+                    peak2 = last;
+                    valley2 = curr;
+                    break;
+                }
+            }
+
+            last = curr;
+        }
+
+        if (peak2 == null) {
+            int val = peak1.val;
+            peak1.val = valley1.val;
+            valley1.val = val;
+        }else{
+            int val = peak1.val;
+            peak1.val = valley2.val;
+            valley2.val = val;
+        }
+    }
+}
